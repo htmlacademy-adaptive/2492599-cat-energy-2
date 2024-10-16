efd36089595158b64eeb3196304a98127c7c04c4
ekaterina.likhacheva@gmail.com <ekaterina.likhacheva@gmail.com>
2024-10-16 22:08:11 +0300

    разметка и стилизация всех страниц

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,25 @@
-/* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.nav');
+const navButton = document.querySelector('.nav__button');
+
+nav.classList.add('nav--closed');
+
+navButton.addEventListener('click', () => {
+  nav.classList.toggle('nav--closed');
+  nav.classList.toggle('nav--opened');
+});
+
+const slider = document.querySelector('.slider');
+const sliderButton = document.querySelector('.slider__toggle');
+
+if (slider) {
+  let flag = false;
+  sliderButton.addEventListener('click', () => {
+    if (!flag) {
+      flag = true;
+      slider.classList.add('slider--before');
+    } else {
+      slider.classList.toggle('slider--before');
+      slider.classList.toggle('slider--after');
+    }
+  });
+}
